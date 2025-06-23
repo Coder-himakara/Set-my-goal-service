5f441e7875b7049b75fb23c42d20f52c85bfd1bc
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-06-23 11:08:50 +0530

    Project initialized

---
+++ src/main/java/coder/himakara/Set_my_goals/SetMyGoalsApplication.java
@@ -0,0 +1,13 @@
+package coder.himakara.Set_my_goals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetMyGoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetMyGoalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coder/himakara/Set_my_goals/SetMyGoalsApplicationTests.java
@@ -0,0 +1,13 @@
+package coder.himakara.Set_my_goals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetMyGoalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
